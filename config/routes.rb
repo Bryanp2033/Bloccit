e537ec09609c2f688c6f2334aa141b5ccb4b73f5
Bryan <bryanporras2033@gmail.com>
2015-08-31 16:33:57 -0400

    Checkpoint 49

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,15 @@ Rails.application.routes.draw do
   resources :advertisements, only: [:index, :show]
   resources :question
 
-     resources :topics do
+       resources :topics do
      resources :posts, except: [:index]
    end
- 
+
    resources :posts, only: [] do
      resources :comments, only: [:create, :destroy]
+
+     post '/up-vote' => 'votes#up_vote', as: :up_vote
+     post '/down-vote' => 'votes#down_vote', as: :down_vote
    end
 
   get 'about' => 'welcome#about'
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,17 +1,19 @@
+require 'rails_helper'
+
 describe Vote do 
-	describe "validations" do
-		describe "value validation" do
-			it "only allows -1 or 1 as values" do
-				v_postive = Vote.new(value: 1)
-				expect (v_postive.valid?).to eq(true)
 
-				v_negative = Vote.new(value: -1)
-				expect (v_negative.valid?).to eq(true)
+  describe "validations" do
+    describe "value validation" do
+      it "only allows -1 or 1 as values" do
+        v = Vote.new(value: 1)
+        expect(v.valid?).to eq(true)
 
-				v_false = Vote.new(value: 4)
-				expect (v_false.valid?).to eq(false)
-			end
-		end
-	end
-	
+        v2 = Vote.new(value: -1)
+        expect(v2.valid?).to eq(true)
 
+        bad_v = Vote.new(value: 2)
+        expect(bad_v.valid?).to eq(false)
+      end
+    end
+  end
+end
