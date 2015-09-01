2870801206b0899bbe99bbe5b46eaf8ec3912422
Bryan <bryanporras2033@gmail.com>
2015-09-01 00:29:15 -0400

    Checkpoint 50

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,8 +11,8 @@ scope :order_by_reverse_created, -> { order('created_at DESC') }
 
 validates :title, length: { minimum: 5 }, presence: true
    validates :body, length: { minimum: 20 }, presence: true
-   #validates :topic, presence: true
-   #validates :user, presence: true
+   validates :topic, presence: true
+   validates :user, presence: true
 
    def up_votes
    	votes.where(value: 1).count
@@ -32,5 +32,10 @@ validates :title, length: { minimum: 5 }, presence: true
 
    		update_attribute(:rank, new_rank)
    	end
-end
+
+   	def create_vote
+   		user.votes.create(value: 1, post: self)
+   	end
+   end
+
 	 
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe Vote do 
+include TestFactories
+Devise::TestHelpers
 
   describe "validations" do
     describe "value validation" do
@@ -16,4 +18,13 @@ describe Vote do
       end
     end
   end
+
+
+   describe 'after_save' do
+     it "calls `Post#update_rank` after save" do
+       vote = Vote.new(value: 1, post: post)
+       expect(post).to receive(:update_rank)
+       vote.save
+     end
+   end
 end
