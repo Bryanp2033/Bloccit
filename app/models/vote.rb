d02825fe22bd209050064a48ed549105dfa31a51
Matthieu Tanguay-Carel <matthieutc@gmail.com>
2015-09-02 10:03:20 -0400

    fixes upvoting

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,14 +2,14 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
-validates :value, inclusion: { in: [-1, 1], message: "%{value} is not a valid vote." }
+  validates :value, inclusion: { in: [-1, 1], message: "%{value} is not a valid vote." }
 
-after_save :update_post
+  after_save :update_post
 
-private
+  private
 
-def update_post
-	post.update_post
-end
+  def update_post
+    post.update_rank
+  end
 
 end
