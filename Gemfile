cb70bed35d0dd6e067bdb00b6391957a8b554478
Bryan <bryanporras2033@gmail.com>
2015-09-06 15:44:57 -0400

    Checkpoint 53

--- Gemfile
+++ Gemfile
@@ -46,7 +46,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
 
-
+  gem 'capybara'
  
   gem 'byebug'
 
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -7,6 +7,14 @@ validates :value, inclusion: { in: [-1, 1], message: "%{value} is not a valid vo
 after_save :update_post
 
 
+ def up_vote?
+     value == 1
+   end
+ 
+   def down_vote?
+     value == -1
+   end
+   
 private
 
 def update_post
