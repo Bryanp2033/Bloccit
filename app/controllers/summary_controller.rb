394210b07c6520f91793439d5f4dbd442d7abb73
Bryan <bryanporras2033@gmail.com>
2015-09-19 14:51:01 -0400

    added some stuff to summary views

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -2,27 +2,27 @@ class SummaryController < ApplicationController
 
 def index
      @summary = Summary.all
-     authorize @summary
+   
    end
 
    def new
      @summary = Summary.new
-     authorize @summary
+    
    end
 
    def show
      @summary = Summary.find(params[:id])
-     authorize @summary
+     
    end
 
    def edit
      @summary = Summary.find(params[:id])
-     authorize @summary
+     
    end
  
    def create
      @summary = Summary.new(params.require(:Summary).permit(:name, :description, :public))
-     authorize @summary
+     
      if @summary.save
        redirect_to @summary, notice: "Summary was saved successfully."
      else
@@ -33,9 +33,9 @@ def index
  
    def update
      @summary = Summary.find(params[:id])
-     authorize @summary
+     
      if @summary.update_attributes(params.require(:summary).permit(:description, :public))
-       redirect_to @summary
+       redirect_to post_summary_path
      else
        flash[:error] = "Error saving Summary. Please try again."
        render :edit
