3bffe8b1d413757cd16e598c1966676a3d0b82ae
Bryan <bryanporras2033@gmail.com>
2015-10-06 13:39:32 -0400

    finished user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,9 @@ describe User do
 
 	describe "#favorited(post)" do
 		it "returns 'nil' if the user has not favorited the post" do
-			user = User.create! email: 'mine@example.com', password: '12345678' # create a user
-			topic = Topic.create! name: 'example', description: 'This is a topic for this spec test'
-			post = Post.create! title: 'This is a title for this', body:'This is a body for the title of this post for this test' # create a post
+			user = User.create email: 'mine@example.com', password: '12345678' # create a user
+			topic = Topic.create name: 'example', description: 'This is a topic for this spec test'
+			post = Post.create title: 'This is a title for this', body:'This is a body for the title of this post for this test' # create a post
 
 		    favorite = user.favorited(post) # call favorited on the user with that post in parameters
 			
@@ -16,14 +16,14 @@ describe User do
 		end
 
 		it "returns the appropriate favorite if it exists" do
-			user = User.create! email: 'mine@example.com', password: '12345678' # create a user
-			topic = Topic.create! name: 'example', description: 'This is a topic for this spec test'
-			post = Post.create! title: 'This is a title for this', body:'This is a body for the title of this post for this test' # create a post
-			favorite = Favorite.create! user:'user_example'# create a favorite for the user for that post
+			user = User.create email: 'mine@example.com', password: '12345678' # create a user
+			topic = Topic.create name: 'example', description: 'This is a topic for this spec test'
+			post = Post.create title: 'This is a title for this', body:'This is a body for the title of this post for this test' # create a post
+			favorite = Favorite.create user: user# create a favorite for the user for that post
 
-			favorited = user.favorite(post)# call favorited on the user with that post in parameters
+			favorited = user.favorited(post)# call favorited on the user with that post in parameters
 
-			expect(favorited).to eq(favorited) # expect the result to be the favorite
+			expect(favorited).to eq(favorite) # expect the result to be the favorite
 		end
 	end
 end
